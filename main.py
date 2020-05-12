61273a7b9c96d4b289f5116e2322a9dfe273c9bb
Mercy1 <tangelmbro@gmail.com>
2020-05-12 19:32:23 +0000

    Formatting

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ chances = 3
 balance = 100.00
 Ibalance = 0
 
-
-
 #Welcome message
 print("******************************************************************")
 print("******************************************************************")
@@ -75,93 +73,75 @@ if PIN == (5981):
                 print("************************************")
             else:
                 print("Unknown 2FA Code")
-                restart = input("Would You you like to use another service? (Y/N)")
+                restart = input(
+                    "Would You you like to use another service? (Y/N)")
                 if restart in ('n', 'NO', 'no', 'N'):
-                  print('Thank You')
-                  break
-        elif option == 6:  
+                    print('Thank You')
+                    break
+        elif option == 6:
             #Menu
-          print("************************************")
-          print("Welcome to Other, what would you like to do?")
-          print("1. Top-up a mobile")
-          print("2. Close an account")
-          print("3. Support")
-          option = int(input("Choose a number-"))
-          if option == 1: #Mobile top-up
-            mobile = int(input("Type your phone number-"))
-            MBalance = int(
-                input("How much would you like to add to the account?-"))
-            MBalance = Ibalance + MBalance
-            Tbalance = balance - MBalance
-            print("After paying your mobile balance is now- £", MBalance)
-            print("******************************")
-            print("******************************")
-            print("Your account balance is now- £", Tbalance)
-            restart = input("Would you like to use another service? (Y/N)")
-            if restart in ('n', 'NO', 'no', 'N'):
-                print('Thank You')
-                break
-          elif option == 2:
-            #closing account
-              print("Closing account.....")
-              print("*************10%**************")
-              print("******************************")
-              print("******************************")
-              print("*************30%**************")
-              print("******************************")
-              print("*************50%**************")
-              print("******************************")
-              print("*************75%**************")
-              print("******************************")
-              print("*************90%**************")
-              print("******************************")
-              print("************DONE**************")
-              
+            print("************************************")
+            print("Welcome to Other, what would you like to do?")
+            print("1. Top-up a mobile")
+            print("2. Close an account")
+            print("3. Support")
+            option = int(input("Choose a number-"))
+            if option == 1:  #Mobile top-up
+                mobile = int(input("Type your phone number-"))
+                MBalance = int(
+                    input("How much would you like to add to the account?-"))
+                MBalance = Ibalance + MBalance
+                Tbalance = balance - MBalance
+                print("After paying your mobile balance is now- £", MBalance)
+                print("******************************")
+                print("******************************")
+                print("Your account balance is now- £", Tbalance)
+                restart = input("Would you like to use another service? (Y/N)")
+                if restart in ('n', 'NO', 'no', 'N'):
+                    print('Thank You')
+                    break
+            elif option == 2:
+                #closing account
+                print("Closing account.....")
+                print("*************10%**************")
+                print("******************************")
+                print("******************************")
+                print("*************30%**************")
+                print("******************************")
+                print("*************50%**************")
+                print("******************************")
+                print("*************75%**************")
+                print("******************************")
+                print("*************90%**************")
+                print("******************************")
+                print("************DONE**************")
 
-
-
-
-
-
-
-
-
-                  
         #elif option == 5:  #option 5
-         #def validatePIN(pin):
-          #attempt=input("Please enter your 4-digit PIN: ")
+        #def validatePIN(pin):
+        #attempt=input("Please enter your 4-digit PIN: ")
         #count=1
     #while (attempt != pin) and (count<3):
-      #attempt=input("Please enter your 4-digit PIN: ")
+    #attempt=input("Please enter your 4-digit PIN: ")
     #count+=1
     #if (attempt == pin):
-     #   return True
+    #   return True
     #print("Invalid PIN - Account Blocked")
     #return False
 
     #def CP(pin):
-     # if validatePIN(pin):
-      #  pin=input("Please enter your new PIN: ")
-       # print("Thank you. Your new PIN is: ",pin)
-      #return pin
+    # if validatePIN(pin):
+    #  pin=input("Please enter your new PIN: ")
+    # print("Thank you. Your new PIN is: ",pin)
+    #return pin
 
     #pin = CP('5981')
     #restart = input("Would you like to use another service? (Y/N)")
     #if restart in ('n', 'NO', 'no', 'N'):
     # print('Thank You')
-  #break
-
-
-
-
-
-
-
-
-
+#break
 
 else:
- print("incorrect PIN remove card and try again.. 2 attempts left")
- input("\nPress Enter to return card ^^ ---")
- print("Thank you for using this ATM... Goodbye")
+    print("incorrect PIN remove card and try again.. 2 attempts left")
+    input("\nPress Enter to return card ^^ ---")
+    print("Thank you for using this ATM... Goodbye")
 ######################################
